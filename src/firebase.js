5ba3367e74e3f8b88a89d0ee3ca77112d5b9afae
millanadhikari <millanadhikari@gmail.com>
2021-03-17 14:46:23 +1100

    React app and Header Created

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFQhaQic3dgr4nEa467JnmFOk741zUzsM",
+    authDomain: "clone-5fbc9.firebaseapp.com",
+    databaseURL: "https://clone-5fbc9.firebaseio.com",
+    projectId: "clone-5fbc9",
+    storageBucket: "clone-5fbc9.appspot.com",
+    messagingSenderId: "674890199145",
+    appId: "1:674890199145:web:d7371a671aa4eb1565f2f8",
+    measurementId: "G-CBY4V6MWYC"
+  };
